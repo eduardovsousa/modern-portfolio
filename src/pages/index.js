c1b1128f58d47ee4cd3109d45e025a1aa37b3b66
Eduardo Varela Sousa <eduardo.varela@outlook.com.br>
2023-04-29 01:52:19 -0300

    change padding in button

--- src/pages/index.js
+++ src/pages/index.js
@@ -50,7 +50,7 @@ export default function Home() {
                   href="/cv.pdf"
                   target={"_blank"}
                   className="flex items-center bg-dark text-light p-2.5 px-6 rounded-lg text-lg font-semibold hover:bg-light hover:text-dark border border-solid border-transparent hover:border-dark
-                  dark:bg-light dark:text-dark hover:dark:bg-dark hover:dark:text-light hover:dark:border-light md:p-2 md:px-4 md:text-base"
+                  dark:bg-light dark:text-dark hover:dark:bg-dark hover:dark:text-light hover:dark:border-light sm:p-1 md:p-2 md:px-4 md:text-base"
                   download={true}
                 >
                   Currículo
